1b98fd003de461246defd187b58251733d760626
spoutshield <shojha@ctemc.org>
2023-05-14 07:39:13 -0400

    added model

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ from flask_cors import CORS
 import base64
 import json
 import pyrebase
-from google.cloud import firestore
 import os
-#from model import get_pred, m
+from PIL import Image#from model import get_pred, m
 from flask_cors import CORS, cross_origin
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
 
 app = Flask(__name__)
 app.config['CORS_HEADERS']='Content-Type'
@@ -21,6 +23,20 @@ with open('config.json') as json_file:
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
+model = tf.keras.models.load_model("celebrity_classification_TFL.h5")
+
+def get_and_process():
+    img = Image.open('img.jpg')
+    img1 = img
+    """Resize image to appropriate shape for model."""
+    img = img.resize((150,150))
+    """Convert img to numpy array,rescale it,expand dims and check vertically."""
+    x = tf.keras.preprocessing.image.img_to_array(img)
+    x = x / 255.0
+    x = np.expand_dims(x,axis = 0)
+    img_tensor = np.vstack([x])
+    return img1,img_tensor
+
 @app.route("/get_img", methods = ['GET', 'POST'])
 def recieve():
   data = request.get_json()
@@ -32,11 +48,22 @@ def recieve():
 
   with open(f"img.jpg", "wb") as fh:
     fh.write(base64.decodebytes(string_bytes))
+  
+  img, img_tens = get_and_process()
+  prediction = model.predict(img_tens)
+
+  classes = ['Angelina Jolie', 'Brad Pitt', 'Denzel Washington', 'Hugh Jackman', 'Jennifer Lawrence', 'Johnny Depp', 'Kate Winslet', 'Leonardo DiCaprio', 'Megan Fox', 'Natalie Portman', 'Nicole Kidman', 'Robert Downey Jr', 'Sandra Bullock', 'Scarlett Johansson', 'Tom Cruise', 'Tom Hanks', 'Will Smith']
+  print(f"Prediction is : {classes[np.argmax(prediction)]}")
+
+  if classes[np.argmax(prediction)] in ["Brad Pitt", 'Denzel Washington', "Jennifer Lawrence", "Johnny Depp", "Tom Cruise"]:
+     whoIsIt = classes[np.argmax(prediction)]
+  else:
+     whoIsIt = "False"
 
   db.child('data/').update(
                 {time: {
                     'img': str(image),
-                    'whoIsIt': "me"
+                    'whoIsIt': whoIsIt
                 }}
             )
 
