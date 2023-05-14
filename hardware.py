2756afbeeb7db633b1150f2b3a98131413654532
spoutshield <shojha@ctemc.org>
2023-05-14 03:12:05 -0400

    audio functionality (hardware basically done)

--- hardware.py
+++ hardware.py
@@ -1,12 +1,16 @@
 import cv2
-import numpy as np
 import time
 import datetime
 import os
 import requests
 import base64
+import pygame
+import random
 
 cap = cv2.VideoCapture(0)
+pygame.init()
+pygame.mixer.music.set_volume(0.5)
+
 
 def convert_to_64(img_path):
     with open(img_path, 'rb') as f:
@@ -33,6 +37,12 @@ while True:
         
     print(response.status_code, response.content)
     
+    """if random.randint(0, 5) == 3:
+        pygame.mixer.music.load("arhan.mp3")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pass"""
+     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     time.sleep(1)
